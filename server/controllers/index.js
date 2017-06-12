48cabb20ada2c952bf46715d7c21af99589eb81b
Hack Reactor Student <student@hackreactor.com>
2017-06-12 16:54:00 -0700

    made it so we can hit the controllers by directly pinging them in the url

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,14 +2,28 @@ var models = require('../models');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+      console.log("It got to the get messages in the controller: ", req.url);
+      res.statusCode = 200;
+      res.end();
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+      res.statusCode = 200;
+      res.end();
+    } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function (req, res) {
+      console.log("It got to the get messages in the controller: ", req.url);
+      res.statusCode = 200;
+      res.end();
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+      res.statusCode = 200;
+      res.end();
+    } // a function which handles posting a message to the database
   }
 };
 
